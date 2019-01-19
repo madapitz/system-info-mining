ac52250fc595b970679651241709c00524fadd8e
Manuel Diaz <32418002+madapitz@users.noreply.github.com>
2019-01-18 22:15:50 -0400

    Add files via upload

--- info.py
+++ info.py
@@ -6,8 +6,8 @@ import uuid
 import psutil
 import os
 import netifaces
-import gpuinfo
-import apt
+if platform.system() == 'Linux':
+	import apt
 
 def getHostName():
 	hostname = socket.gethostname()
@@ -32,13 +32,15 @@ def getSerialNumber(os):
 		return subprocess.check_output(['sudo', 'dmidecode', '-s', 'system-serial-number' ]).decode("utf-8").strip('\n')
 	elif os == 'Windows':
 		#si retorna 0 es culpa del fabricante de la maquina
-		return subprocess.check_output(['wmic', 'bios', 'get', 'serialnumber' ])
+		sn = subprocess.check_output(['wmic', 'bios', 'get', 'serialnumber' ]).decode("utf-8").strip('\r\r\n')
+		return re.findall(r'[^SerialNumber\r\n ].*\w\S',sn)[0]
 
 def getManufacturer(os):
 	if os == 'Linux':
 		return subprocess.check_output(['sudo', 'dmidecode', '-s', 'system-manufacturer' ]).decode("utf-8").strip('\n')
 	elif os == 'Windows':
-		return 'por investigar'
+		manu = subprocess.check_output(['wmic', 'computersystem', 'get', 'manufacturer']).decode("utf-8").strip('\n')
+		return re.findall(r'[^Manufacturer\r\n ].*\w\S',manu)[0]
 
 def getOS():
 	return platform.system()
@@ -52,39 +54,65 @@ def getOSVersion(os):
 def getModel(os):
 	if os == 'Linux':
 		return subprocess.check_output(['sudo', 'dmidecode', '-s', 'system-version' ]).decode("utf-8").strip('\n')
+	elif os == 'Windows':
+		model = subprocess.check_output(['wmic', 'computersystem', 'get', 'model']).decode("utf-8").strip('\n')
+		return re.findall(r'[^Model\r\n ].*\w\S',model)[0]
 
 #solo para windows
 def getSP():
-	return platform.win32_ver().csd
+	return platform.win32_ver()[2]
 
 def getMAC():
 	return ':'.join(re.findall('..', '%012x' % uuid.getnode()))
 
 def getRAMInfo():
 	#hacer version para windows
-	ramname = subprocess.check_output(['sudo', 'dmidecode', '-t', '17']).decode("utf-8").strip('\n')
-	info = {
-		'nombre': re.findall('DDR', ramname)[0] + ramname[810],
-		'total':psutil.virtual_memory().total / 1048576,
-		'unidad': 'MB',
-		'tipo': 'RAM',
-		'disponible': psutil.virtual_memory().available / 1048576
-	}
+	if platform.system() == 'Linux':
+		ramname = subprocess.check_output(['sudo', 'dmidecode', '-t', '17']).decode("utf-8").strip('\n')
+		info = {
+			'nombre': re.findall('DDR', ramname)[0] + ramname[810],
+			'total':psutil.virtual_memory().total / 1048576,
+			'unidad': 'MB',
+			'tipo': 'RAM',
+			'disponible': psutil.virtual_memory().available / 1048576
+		}
 
-	return info
+		return info
+	elif platform.system() == 'Windows':
+		ram = subprocess.check_output(['wmic','memorychip','get','MemoryType']).decode("utf-8").strip(' \r\n')
+
+		info = {
+			'nombre': re.findall(r'[^MemoryType\r\n ].*\S',ram)[0],
+			'total':psutil.virtual_memory().total / 1048576,
+			'unidad': 'MB',
+			'tipo': 'RAM',
+			'disponible': psutil.virtual_memory().available / 1048576
+		}
+
+		return info
 
 def getHardDiskInfo():
 	#os.getenv("SystemDrive") para windows
-	tipo = subprocess.check_output(['cat', '/sys/block/sda/queue/rotational']).decode("utf-8").strip('\n')
-	if tipo == '1':
-		tipoD = 'HDD'
-	else:
-		tipoD = 'SDD'
+	if platform.system() == 'Linux':
+		tipo = subprocess.check_output(['cat', '/sys/block/sda/queue/rotational']).decode("utf-8").strip('\n')
+		if tipo == '1':
+			tipoD = 'HDD'
+		else:
+			tipoD = 'SDD'
+	elif platform.system() == 'Windows':
+		tipo = subprocess.check_output(['wmic', 'diskdrive', 'get', 'caption']).decode("utf-8").strip('\n')
+		gold = re.findall(r'[SDD|HDD]',tipo)[0]
+		if gold == []:
+			tipoD = 'unknown'
+		else:
+			tipoD = gold
 
 	for i in psutil.disk_partitions():
 		if i.mountpoint == '/':
 			nombre = i.device
 			break
+		else:
+			nombre = '/'
 
 	info = {
 		'nombre': nombre,
@@ -124,15 +152,28 @@ def getNetworkInterfaces():
 
 def getGPUinfo():
 	#hacer version de windows
-	cap = subprocess.check_output(["lspci", "-vnn"]).decode("utf-8").strip('\n')
-	resl = subprocess.check_output(["sudo","lshw", "-C","display"]).decode("utf-8").strip('\n')
-	info = {
-		'capacidad':re.findall(r"[0-9]..",re.findall(r"\bsize.*M\b",cap)[1])[0],
-		'unidad': 'MB',
-		'resolucion': re.split('\s',re.findall(r"\bwidth:.*s\b",resl)[0])[1] + ' ' + re.split('\s',re.findall(r"\bwidth:.*s\b",resl)[0])[2]
-	}
+	if platform.system() == 'Linux':
+		cap = subprocess.check_output(["lspci", "-vnn"]).decode("utf-8").strip('\n')
+		resl = subprocess.check_output(["sudo","lshw", "-C","display"]).decode("utf-8").strip('\n')
+		info = {
+			'capacidad':re.findall(r"[0-9]..",re.findall(r"\bsize.*M\b",cap)[1])[0],
+			'unidad': 'MB',
+			'resolucion': re.split('\s',re.findall(r"\bwidth:.*s\b",resl)[0])[1] + ' ' + re.split('\s',re.findall(r"\bwidth:.*s\b",resl)[0])[2]
+		}
 
-	return info
+		return info
+	elif platform.system() == 'Windows':
+		precap = subprocess.check_output(["wmic", "path", "win32_VideoController", "get", "adapterram"]).decode("utf-8").strip('\n')
+		cap = re.findall(r'[^AdapterRam\r\n ].*\w\S',precap)[0]
+		preresl = subprocess.check_output(["wmic", "path", "win32_VideoController", "get", "currentverticalresolution"]).decode("utf-8").strip('\n')
+		resl = re.findall(r'[^CurrentVerticalResolution\r\n ].*\w\S',preresl)[0]
+		info = {
+			'capacidad':str(int(cap)/1048576),
+			'unidad': 'MB',
+			'resolucion': resl
+		}
+
+		return info
 
 def getPrinters(so):
 	if so == 'Linux':
@@ -147,9 +188,31 @@ def getPrinters(so):
 				})
 
 		return prList
+	elif so == 'Windows':
+		prList = []
+		preport = subprocess.check_output(["wmic", "printer","get", "portname"]).decode("utf-8").strip('\n')
+		ports = re.findall(r'[^PortNameXPSPortHRFAXnul\r\n ].*\w\S',preport)
+
+		pred = subprocess.check_output(["wmic", "printer","get", "drivername"]).decode("utf-8").strip('\n')
+		drivers = re.findall(r'[^DriverName\r\n ].*\w\S',pred)
+
+		for x in drivers:
+			prList.append({
+				'puerto': 'none',
+				'driver': x
+				})
+		
+		if len(drivers) > 3:
+			for y in range(3,len(drivers)):
+				if y - 3 < len(ports):
+					prList[y]['puerto'] = ports[y - 3]
+
+
+		return prList
 
 def getAllInstalledApps(so):
 	#2191
+	# nombre, categoria, clave, fecha inst, fabricante
 	if so == "Linux":
 		AppName = []
 
@@ -160,6 +223,26 @@ def getAllInstalledApps(so):
 		        AppName.append(mypkg.name)
 		
 		return AppName
+	elif so == "Windows":
+		app = []
+		nm = subprocess.check_output(["wmic", "product","get", "name"]).decode("utf-8").strip('\n')
+		name = re.findall(r'[^Name\r\n ].*\w\S',nm)
+		cv = subprocess.check_output(["wmic", "product","get", "IdentifyingNumber"]).decode("utf-8").strip('\n')
+		clave = re.findall(r'[^IdentifyingNumber\r\n ].*\w\S',cv)
+		fi = subprocess.check_output(["wmic", "product","get", "installdate"]).decode("utf-8").strip('\n')
+		fecha = re.findall(r'[^InstallDate\r\n ].*\w\S',fi)
+		vd = subprocess.check_output(["wmic", "product","get", "vendor"]).decode("utf-8").strip('\n')
+		vendor = re.findall(r'[^Vendor\r\n ].*\w\S',vd)
+
+		for x in range(0,len(name)-1):
+			app.append({
+				'nombre':name[x],
+				'clave':clave[x],
+				'fecha': fecha[x],
+				'fabricante': vendor[x]
+				})
+
+		return app
 
 def exportSystemInfo():
 	f = open('info.txt','w')
@@ -224,9 +307,17 @@ def exportInstalledApps():
 	apps = getAllInstalledApps(platform.system())
 	f = open('info.txt','a')
 	f.write('\nSoftware:\n')
-	for x in apps:
-		f.write('nombre: ' + x + '\n')
-		f.write('-------\n')
+	if platform.system() == 'Linux':
+		for x in apps:
+			f.write('nombre: ' + x + '\n')
+			f.write('-------\n')
+	elif platform.system() == 'Windows':
+		for x in apps:
+			f.write('nombre: ' + x['nombre'] + '\n')
+			f.write('clave: ' + x['clave'] + '\n')
+			f.write('fecha instalacion: ' + x['fecha'] + '\n')
+			f.write('fabricante: ' + x['fabricante'] + '\n')
+			f.write('-------\n')
 
 def exportAllInfo():
 	exportSystemInfo()
@@ -238,5 +329,5 @@ def exportAllInfo():
 
 if __name__ == '__main__':
 	#grep " install" /var/log/dpkg.log*
+	#wmic computersystem get model
 	exportAllInfo()
-	
